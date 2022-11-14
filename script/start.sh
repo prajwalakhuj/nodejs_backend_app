1a190be7a314cf6725ad1817c3ae64eac92cb6b4
prajwalakhuj <116149622+prajwalakhuj@users.noreply.github.com>
2022-11-14 14:16:56 +0530

    added 'git pull' command

--- script/start.sh
+++ script/start.sh
@@ -2,5 +2,7 @@
 # Changing required permissions
 sudo chmod -R 777 /home/ubuntu/nodejs_backend_app
 cd /home/ubuntu/nodejs_backend_app
+# to sync the changes
+git pull
 # Starting the nodejs app using pm2
 pm2 start app.js
