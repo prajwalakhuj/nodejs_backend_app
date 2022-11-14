969150d67fee77c65f53315affba23ca4f0ff33d
prajwalakhuj <116149622+prajwalakhuj@users.noreply.github.com>
2022-11-14 14:06:13 +0530

    updated path of repository

--- script/start.sh
+++ script/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Changing required permissions
-sudo chmod -R 777 /home/ubuntu/node-express-realworld-example-app
-cd /home/ubuntu/node-express-realworld-example-app
+sudo chmod -R 777 /home/ubuntu/nodejs_backend_app
+cd /home/ubuntu/nodejs_backend_app
 # Starting the nodejs app using pm2
 pm2 start app.js
