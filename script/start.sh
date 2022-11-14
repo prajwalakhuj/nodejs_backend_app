ecb75bf4e61ea08e55be434f9fa3fe3085f8ce22
prajwalakhuj <116149622+prajwalakhuj@users.noreply.github.com>
2022-11-14 19:43:05 +0530

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -2,7 +2,5 @@
 # Changing required permissions
 sudo chmod -R 777 /home/ubuntu/nodejs_backend_app
 cd /home/ubuntu/nodejs_backend_app
-# to sync the changes
-git pull
 # Starting the nodejs app using pm2
 pm2 start app.js
