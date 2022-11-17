72cd7ab060c9dd5691d2c18d227cf56310944032
prajwalakhuj <116149622+prajwalakhuj@users.noreply.github.com>
2022-11-17 19:34:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,6 +39,13 @@ if(isProduction){
   mongoose.set('debug', true);
 }
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
+
 require('./models/User');
 require('./models/Article');
 require('./models/Comment');
