5d387a7cab8ccab7a539fedf41ece2eb1e0027cc
prajwalakhuj <116149622+prajwalakhuj@users.noreply.github.com>
2022-11-17 16:34:38 +0530

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # updating the linux
 apt update && apt upgrade -y
-apt update && sudo apt upgrade -y
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-apt install nodejs nginx -y
-npm install npm@latest -g
-npm install pm2@latest -g
-npm install --save dotenv
